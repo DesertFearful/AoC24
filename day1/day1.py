d6020624f9c5270dfc849b97602e182f0f1a8bae
Darius Mardaru <dariusmardaru16@gmail.com>
2024-12-01 12:50:59 +0100

    Solved 2nd problem of day 1.

--- day1/day1.py
+++ day1/day1.py
@@ -1,18 +1,35 @@
-def day1p1(file_name):
-    nums1, nums2, res = [], [], 0
+def preprocess(file_name):
+    a, b = [], []
 
     with open(file_name, 'r') as file:
         for line in file:
             row = line.split()
-            nums1.append(int(row[0]))
-            nums2.append(int(row[1]))
-
-    nums1.sort(), nums2.sort()
+            a.append(int(row[0]))
+            b.append(int(row[1]))
     
-    for i in range(len(nums1)):
-        res += abs(nums1[i] - nums2[i])
+    return a, b
 
+
+def day1p1(a, b):
+    a.sort(), b.sort()
+    res = 0
+    for i in range(len(a)):
+        res += abs(a[i] - b[i])
     print(res)
 
+
+def day1p2(a, b):
+    table = {}
+    res = 0
+    for i in range(len(b)):
+        table[b[i]] = 1 + table.get(b[i], 0)
+    for i in range(len(a)):
+        if a[i] in table:
+            res += a[i] * table[a[i]]
+    print(res)
+
+
 if __name__ == '__main__':
-    day1p1('day1/input.txt')
+    nums1, nums2 = preprocess('day1/input.txt')
+    day1p1(nums1, nums2)
+    day1p2(nums1, nums2)
